626415fead45619127090636f93da83901d03b3f
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-01-20 22:48:40 +0700

    Menambahkan ordered list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello Tiya</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam nam aliquid eveniet non incidunt sapiente laborum, velit a vel eaque veritatis molestias debitis at? Voluptas eveniet quis libero optio laudantium mollitia facilis. Suscipit recusandae facilis dolor, odit rerum repellat possimus, corporis sit, eos quo porro. Distinctio, officia beatae! Culpa, voluptate! Laudantium nobis a perferendis minus distinctio. Commodi, magnam laudantium provident ex est pariatur illum laborum, recusandae quo optio excepturi aperiam ipsam aliquam necessitatibus distinctio animi expedita impedit saepe aliquid officia sint fuga, deserunt perferendis? Cumque modi voluptas, repellendus cupiditate quod dolorum officia cum, quaerat ipsa obcaecati consequuntur odio! Nam, voluptatibus.</p>
+
+    <ol>
+        <li>Dede</li>
+        <li>Dewi</li>
+    </ol>
 </body>
 </html>
